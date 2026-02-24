51b35255223e06c149e387825df26fb2080739da
Amish Waqar <waqaramish24@gmail.com>
2026-02-24 10:30:04 +0530

    2nd lec

---
+++ loop.js
@@ -0,0 +1,7 @@
+var count;
+document.write("Starting Loop"+"<br/>");
+for(count=0;count<10;count++){
+    document.write("Current Count:"+count);
+    document.write("<br/>");
+}
+document.write("Loop stopped!");
