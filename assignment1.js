f0b0aa05d0f418946e0b9fb00231bea0f11819dc
Amish Waqar <waqaramish24@gmail.com>
2026-02-27 12:09:01 +0530

    3rd lec

---
+++ assignment1.js
@@ -0,0 +1,10 @@
+var sayHello = function () {
+alert('hello world!');
+}
+var numberArray = [1, 2, 3];
+var animals = new Array("cat", "dog", "mouse", "lion");
+var person = {
+name: 'Barack Hussein Obama II',
+age: '51',
+title: '44th President of the United States'
+}
