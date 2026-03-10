8085e6f45172496f2edb9c9b0bb4a13af2df52e8
Amish Waqar <waqaramish24@gmail.com>
2026-03-10 09:45:43 +0530

    4th lec

---
+++ array.js
@@ -0,0 +1,17 @@
+let myinfo = new Array();
+let myinfo1 = Array();
+
+console.log(myinfo, myinfo1);
+
+let collection = [
+    [],
+    {},
+    true,
+    "john",
+    function () {},
+    3734737,
+    undefined,
+    null,
+    new String("abc"),
+    new Date()
+];
